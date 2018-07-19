665eca3e6e89fdcd98860decad9cbb3069cb13ce
chenweizhi <chenweizhi@blued.com>
2018-07-19 10:41:23 +0800

    init

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'qiubai',
+    USERNAME: 'root',
+    PASSWORD: 'root',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
